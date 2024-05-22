86606ef895ff01efe378ce60d788181cb0d8f065
jieuning <balllling92@gmail.com>
2024-05-22 22:57:24 +0900

    feat: 레시피 필터 기능 구현

---
+++ views/public/js/tabBar.js
@@ -0,0 +1,42 @@
+export const PublicTabBar = () => {
+    const createNavElement = document.createElement("nav");
+    createNavElement.className = "tab_bar";
+
+    createNavElement.innerHTML = `
+        <div class="tab_bar_wrap">
+            <div class="tab">
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24">
+                        <path fill="#e1e1e1" stroke="#e1e1e1" stroke-linecap="round" stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M20 19v-8.5a1 1 0 0 0-.4-.8l-7-5.25a1 1 0 0 0-1.2 0l-7 5.25a1 1 0 0 0-.4.8V19a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1" />
+                    </svg>
+                    <h1 class="tab_title">홈</h1>
+                </a>
+            </div>
+            <div class="tab">
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24">
+                        <g fill="none">
+                            <path
+                                d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                            <path fill="#e1e1e1"
+                                d="M10.92 2.868a1.25 1.25 0 0 1 2.16 0l2.795 4.798l5.428 1.176a1.25 1.25 0 0 1 .667 2.054l-3.7 4.141l.56 5.525a1.25 1.25 0 0 1-1.748 1.27L12 19.592l-5.082 2.24a1.25 1.25 0 0 1-1.748-1.27l.56-5.525l-3.7-4.14a1.25 1.25 0 0 1 .667-2.055l5.428-1.176z" />
+                        </g>
+                    </svg>
+                    <h1 class="tab_title">레시피</h1>
+                </a>
+            </div>
+            <div class="tab">
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24">
+                        <path fill="#e1e1e1" d="M17 3H7a2 2 0 0 0-2 2v16l7-3l7 3V5a2 2 0 0 0-2-2" />
+                    </svg>
+                    <h1 class="tab_title">북마크</h1>
+                </a>
+            </div>
+        </div>
+    `;
+
+    document.body.append(createNavElement);
+};
--- views/recipe/recipe.js
+++ views/recipe/recipe.js
@@ -1,12 +1,114 @@
-const recipeData = async () => {
-    const url = 'http://localhost:3000/api/basic';
+import { PublicTabBar } from "../public/js/tabBar.js";
+
+
+const itemFilters = document.querySelectorAll(".recipe_filter span");
+let currentTarget = itemFilters[0];
+
+const fetchData = async () => {
+    const API_KEY = '66c340d78ebbaf115f7216a55a2b2de11e2a215b696439ef449586096f885f49';
+    const url = new URL(`http://211.237.50.150:7080/openapi/${API_KEY}/json/Grid_20150827000000000226_1/1/10`);
+    console.log(url);
 
     try{
-        const data = await fetch(url).then((res) => res.json());
+        const res = await fetch(url);
+        if(!res.ok) {
+            throw new Error(`에러 발생: ${res.status}`);
+        }
 
-        console.log(data);
+        const result = await res.json();
+        const data = result.Grid_20150827000000000226_1.row;
+        currentTarget.classList.add("on");
+
+        // 낮은 난이도 순으로 정렬
+        const levelArr = ['초보환영', '보통', '어려움'];
+        const lowLevel = data.sort((a, b) => {
+            return levelArr.indexOf(a.LEVEL_NM) - levelArr.indexOf(b.LEVEL_NM);
+        });
+        
+        for(let i=0; i<itemFilters.length; i++) {
+            itemFilters[i].addEventListener("click", (e) => handleClickFilter(e, data));
+        };
+
+        recipeListRender(lowLevel);
     }catch(error) {
         console.log(error.message);
     }
 };
-recipeData();
+fetchData();
+
+const recipeListHtml = (data) => {
+    return `
+        <li class="recipe_list">
+            <div class="img_wrap">
+                <a href="#">
+                    <img src="/image/fake_img.png"/>
+                </a>
+            </div>                
+            <article class="recipe_contents">
+                <div class="title_wrap">
+                    <h2 class="recipe_title">${data.RECIPE_NM_KO}</h2>
+                    <p class="recipe_Summary">${data.SUMRY}</p>
+                </div>
+                <div class="recipe_tags">
+                    <span class="level">${data.LEVEL_NM}</span>
+                    <span class="cooking_time">${data.COOKING_TIME}</span>
+                    <span class="bookmark"></span>
+                </div>
+            </article>
+        </li>
+    `
+};
+
+const recipeLists = document.querySelector(".recipe_lists");
+
+const recipeListRender = (soltedData, target = currentTarget) => {
+    recipeLists.innerHTML = '';
+
+    currentTarget.classList.remove("on");
+    target.classList.add("on");
+    currentTarget = target;
+
+    soltedData.forEach(data => {
+        recipeLists.innerHTML += recipeListHtml(data);
+    });
+};
+
+const handleClickFilter = (event, data) => {
+    let target = event.target;
+    let soltedData = soltedFilter(data, target);
+
+    recipeListRender(soltedData, target);
+};
+
+const soltedFilter = (data, target) => {
+    const levelArr = ['초보환영', '보통', '어려움'];
+    const targetValue = target.innerText;
+
+    if(targetValue === "낮은 난이도") {
+        const lowLevel = data.sort((a, b) => {
+            return levelArr.indexOf(a.LEVEL_NM) - levelArr.indexOf(b.LEVEL_NM);
+        });
+        return lowLevel;
+    } else if(targetValue === "높은 난이도") {
+        const highLevel = data.sort((a, b) => {
+            return levelArr.indexOf(b.LEVEL_NM) - levelArr.indexOf(a.LEVEL_NM);
+        });
+        return highLevel;
+    } else if(targetValue === "짧은 조리시간") {
+        const lowTime = data.sort((a, b) => {
+            return Number(a.COOKING_TIME.slice(0, -1)) - Number(b.COOKING_TIME.slice(0, -1));
+        });
+        return lowTime;
+    } else {
+        const highTime = data.sort((a, b) => {
+            return Number(b.COOKING_TIME.slice(0, -1)) - Number(a.COOKING_TIME.slice(0, -1));
+        });
+        return highTime;
+    }
+};
+
+const tabBarRender = () => {
+    return PublicTabBar();
+};
+tabBarRender();
+
