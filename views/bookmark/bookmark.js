9436e10fd1033e7596314706765257c90d2f64c7
JIEUN <108172664+jieuning@users.noreply.github.com>
2024-06-01 12:53:22 +0900

    fix: #1 특정 북마크 삭제시 생긴 문제 해결

--- views/bookmark/bookmark.js
+++ views/bookmark/bookmark.js
@@ -11,6 +11,7 @@ let activeData = [];
 
 const bookmarkData = () => {
   const storageKeys = Object.keys(localStorage);
+
   // 로컬스토리지에서 ingredient 제외
   const ingredientRemove = storageKeys.filter(
     (string) => string !== "ingredient"
@@ -22,21 +23,12 @@ const bookmarkData = () => {
   });
 
   markedItemRender(activeData);
-
-  const bookmarks = document.querySelectorAll(".bookmark");
-
-  for (let i = 0; i < bookmarks.length; i++) {
-    bookmarks[i].classList.add("on");
-    bookmarks[i].addEventListener("click", () => {
-      let bookmarkId = bookmarks[i].getAttribute("data-id");
-      clickBookmarkBtn(i, bookmarks, activeData, bookmarkId); // 필터링된 데이터 전달
-    });
-  }
 };
 
 const bookmarkLists = document.querySelector(".item_lists");
 
 const markedItemRender = (activeData) => {
+  // 재렌더링시 초기화
   bookmarkLists.innerHTML = "";
 
   if (activeData.length !== 0) {
@@ -51,40 +43,25 @@ const markedItemRender = (activeData) => {
         </div>
     `;
   }
+
+  // 북마커가 삭제된 후에 새로운 activeData가 재렌더링되더라도
+  // 북마커의 상태가 유지되도록
+  const bookmarks = document.querySelectorAll(".bookmark");
+
+  bookmarks.forEach((bookmark) => {
+    bookmark.classList.add("on");
+    bookmark.addEventListener("click", () => {
+      let bookmarkId = bookmark.getAttribute("data-id");
+      clickBookmarkBtn(bookmarkId);
+    });
+  });
 };
 
-const clickBookmarkBtn = (i, bookmarks, data, bookmarkId) => {
-  const target = bookmarks[i];
-
-  // 클릭된 api 데이터의 레시피 id 값과 클릭한 리스트의 레시피 아이디 값이 같은 데이터
-  // const filteredItem = data.filter((item) => item.RECIPE_ID == bookmarkId);
-
-  // 사용할 데이터만 추출
-  // const usingData = [
-  //   {
-  //     CALORIE: filteredItem[0].CALORIE,
-  //     COOKING_TIME: filteredItem[0].COOKING_TIME,
-  //     LEVEL_NM: filteredItem[0].LEVEL_NM,
-  //     QNT: filteredItem[0].QNT,
-  //     RECIPE_ID: filteredItem[0].RECIPE_ID,
-  //     RECIPE_NM_KO: filteredItem[0].RECIPE_NM_KO,
-  //     SUMRY: filteredItem[0].SUMRY,
-  //   },
-  // ];
-
-  // 필터로인해 인덱스가 일정하지 않음
-  // 새로고침시 클릭된 북마커의 순서를 유지하기 위해 key 값에 레시피 id부여
-  // localStorage.getItem(`bookmark-${bookmarkId}`);
-
-  // if (target.classList.contains("on")) {
-  //   // 업데이트
-  //   localStorage.setItem(`bookmark-${bookmarkId}`, JSON.stringify(usingData));
-  // } else {
+const clickBookmarkBtn = (bookmarkId) => {
   // 토글이 false면 해당 항목 제거
   localStorage.removeItem(`bookmark-${bookmarkId}`);
 
   activeData = [];
-  // bookmarkLists.innerHTML = "";
 
   // 삭제된 아이템을 뺀 나머지 재 렌더링
   const storageKeys = Object.keys(localStorage);
@@ -99,20 +76,7 @@ const clickBookmarkBtn = (i, bookmarks, data, bookmarkId) => {
     activeData.push(localData[0]);
   });
 
-  // activeData.forEach((data) => {
-  //   bookmarkLists.innerHTML += bookmarkCardHtml(data);
-  // });
-
-  // if (activeData.length === 0) {
-  //   bookmarkLists.innerHTML = `
-  //     <div class="empty_wrap">
-  //         <img class="empty_icon" src="../../image/egg_icon.png" />
-  //         <span class="empty_title">마음에 드는 레시피를<br/>지금 바로 추가해보세요.</span>
-  //     </div>
-  // `;
-  // }
   markedItemRender(activeData);
-  // }
 };
 
 const bookmarkCardHtml = (data) => {
