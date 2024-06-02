85024e26cdcbfdaf7028ba1ee9622cbc9de30e97
JIEUN <108172664+jieuning@users.noreply.github.com>
2024-06-02 20:36:08 +0900

    feat: 재료의 id와 일치하는 레시피 기본 정보 가져오기

--- views/customized/customized.js
+++ views/customized/customized.js
@@ -10,9 +10,9 @@ tabBarRender();
 // 추가한 재료가 포함된 레피시를 Id를 통해 불러온다.
 // 그럼 id는 어떻게 얻지? -> 레시피 재료 정보에서 해당 재료가 있으면 Id를 불러온다. -> id를 가지고 레시피 기본 정보를 불러온다.
 // fetch를 총 두 번? 1. 레시피 재료 정보 fetch -> 2. 레시피 기본 정보 fetch
-const fetchData = async (ingredients) => {
-  const API_KEY =
-    "66c340d78ebbaf115f7216a55a2b2de11e2a215b696439ef449586096f885f49";
+const API_KEY =
+  "66c340d78ebbaf115f7216a55a2b2de11e2a215b696439ef449586096f885f49";
+const fetchIngredData = async (ingredients) => {
   const ingredRes = await fetch(
     `http://211.237.50.150:7080/openapi/${API_KEY}/json/Grid_20150827000000000227_1?IRDNT_NM=${ingredients}`
   );
@@ -21,32 +21,30 @@ const fetchData = async (ingredients) => {
   }
   const result = ingredRes.json();
   return result;
-
-  //   const basicUrl = new URL(
-  //     `http://211.237.50.150:7080/openapi/${API_KEY}/json/Grid_20150827000000000226_1?RECIPE_ID=${pageParams}`
-  //   );
-  // const result = await ingredRes.json();
-  // const basicRes = await fetch(basicUrl)
-
-  // 병렬처리, json 파싱
-  // const allRes = await Promise.all([basicRes, ingredRes]);
-  // const [basicResult, recipeResult, ingredResult] = await Promise.all(
-  //   allRes.map((res) => res.json())
-  // );
-
-  // 사용할 데이터
-  // const basicData = basicResult.Grid_20150827000000000226_1.row;
-  // const recipeData = recipeResult.Grid_20150827000000000228_1.row;
-  // const ingredData = ingredResult.Grid_20150827000000000227_1.row;
-  // console.log(basicData);
 };
 
-const fetchAllData = async (ingredients) => {
-  const promises = ingredients.map((ingredient) => fetchData(ingredient));
+const fetchIngredAllData = async (ingredients) => {
+  const promises = ingredients.map((ingredient) => fetchIngredData(ingredient));
   return Promise.all(promises);
 };
 
-const getIngredData = async () => {
+const fetchBaseData = async (id) => {
+  const baseRes = await fetch(
+    `http://211.237.50.150:7080/openapi/${API_KEY}/json/Grid_20150827000000000226_1?RECIPE_ID=${id}`
+  );
+  if (!baseRes.ok) {
+    throw new Error(response.statusText);
+  }
+  const result = baseRes.json();
+  return result;
+};
+
+const fetchBaseAllData = async (recipeId) => {
+  const promises = recipeId.map((id) => fetchBaseData(id));
+  return Promise.all(promises);
+};
+
+const getRecipeData = async () => {
   try {
     let ingredients = [];
     const getIngred = JSON.parse(localStorage.getItem("ingredient"));
@@ -55,7 +53,7 @@ const getIngredData = async () => {
       ingredients.push(ingred.ingredient);
     });
 
-    const result = await fetchAllData(ingredients);
+    const result = await fetchIngredAllData(ingredients);
 
     if (result !== undefined) {
       let recipeId = new Set();
@@ -71,10 +69,11 @@ const getIngredData = async () => {
       });
       recipeId = Array.from(recipeId);
 
-      console.log(recipeId);
+      const baseData = await fetchBaseAllData(recipeId);
+      console.log(baseData);
     }
   } catch (error) {
     console.log(error.message);
   }
 };
-getIngredData();
+getRecipeData();
